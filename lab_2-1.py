c535e6a504044f9b0941457b7be7a953122c60dc
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-09-28 10:04:19 -0400

    Add file

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# Author JRI 9/28/21
+
+x = input("please input an integer")
+
+if int(x) % 2 == 0:
+    print(x, "is an even number")
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author JRI 9/28/21
+
+x = input("please give an integer")
+
+if int(x) % 2 == 0:
+    print(x + " is an even number.")
+else:
+    print(x + " is an odd number.")
