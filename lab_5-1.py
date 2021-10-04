d10a5c4f66e558c33a6641bb04a157d55856aa71
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-10-04 09:57:50 -0400

    Add file

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author JRI 10/4/21
+
+# int(a)
+# Name Error
+
+# int('a')
+# Value Error
+
+# 'a'+2
+# typeerror
+
+# import date
+# Modulenotfounderror
+
+# print("I'm a happy camper!)
+# Syntax Error
